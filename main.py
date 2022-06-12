c98230beb28d791196c5487185fcd6e8b6add56c
rufaro nedziwe <101288323+Rufaro-Nedziwe@users.noreply.github.com>
2022-06-11 19:16:57 -0500

    Add Pong-game initial files

---
+++ main.py
@@ -0,0 +1,48 @@
+import time
+from turtle import Turtle, Screen
+from pong import Pong
+from ball import Ball
+from scoreboard import Scoreboard
+screen = Screen()
+
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("My Pong Game")
+screen.tracer(0)
+
+r_pong = Pong((350, 0))
+l_pong = Pong((-350, 0))
+ball = Ball()
+scoreboard = Scoreboard()
+
+screen.listen()
+screen.onkey(r_pong.go_up, "Up")
+screen.onkey(r_pong.go_down, "Down")
+screen.onkey(l_pong.go_up, "w")
+screen.onkey(l_pong.go_down, "s")
+
+game_is_on = True
+while game_is_on:
+    screen.update()
+    time.sleep(ball.move_speed)
+    ball.move()
+
+    # Detect collision with wall
+    if ball.ycor() > 280 or ball.ycor() < -280:
+        ball.bounce_y()
+
+    # Detect collision with paddle
+    if ball.distance(r_pong) < 50 and ball.xcor() > 320 or ball.distance(l_pong) < 50 and ball.xcor() < -320:
+        ball.bounce_x()
+
+    if ball.xcor() > 380:
+        ball.reset_position()
+        scoreboard.l_point()
+
+    if ball.xcor() < -380:
+        ball.reset_position()
+        scoreboard.r_point()
+
+
+
+screen.exitonclick()
